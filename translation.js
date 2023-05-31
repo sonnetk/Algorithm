273a9dd0c7334b4e87e33e45d467de872aeff791
anarh <anarhist290@mail.ru>
2023-05-31 21:36:58 +0300

    Добавляет проверки ошибок в блок-схеме

--- translation.js
+++ translation.js
@@ -1,62 +1,85 @@
 document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('toCode').addEventListener('click', () => {
+        function ShapeToCode(shape) {
+            switch (shape.figure) {
+                case 'Start':
+                    code += `begin\n`
+                    break
+                case 'Action':
+                    code += `${shape.text.replace('=', ':=')};\n`
+                    break
+                case 'Input':
+                    code += `readln(${shape.text});\n`
+                    break
+                case 'Output':
+                    code += `writeln(${shape.text});\n`
+                    break
+                case 'LoopStart':
+                    code += `for ${shape.text.replace('=', ':=').replace(',', ' to')} do\nbegin\n`
+                    break
+                case 'LoopEnd':
+                    code += `end;\n`
+                    break
+                case 'End':
+                    code += `end.`
+                    break
+            }
+        }
+
         const model = JSON.parse(myDiagram.model.toJson())
         const shapes = model.nodeDataArray
         const links = model.linkDataArray
 
-        let pascal_code = ''
+        let code = ''
 
-        let shape = shapes.find(item => item.figure === 'Start');
-        if (!shape) return
+        let start = shapes.filter(item => item.figure === 'Start')
+        if (start.length === 0) {
+            alert('Не найден блок "Начало"')
+            return
+        }
+        if (start.length > 1) {
+            alert('Обнаружены несколько блоков "Начало"')
+            return
+        }
+        let shape = start[0]
 
-        do {
-            if (shape.figure === 'Start') {
-                pascal_code += `begin
-`
-            }
-
-            if (shape.figure === 'Action') {
-                pascal_code += `${shape.text.replace('=', ':=')};
-`
-            }
-
-            if (shape.figure === 'Input') {
-                pascal_code += `readln(${shape.text});
-`
-            }
-
-            if (shape.figure === 'Output') {
-                pascal_code += `writeln(${shape.text});
-`
-            }
-
-            if (shape.figure === 'LoopStart') {
-                pascal_code += `for ${shape.text.replace('=', ':=').replace(',', ' to')} do
-begin
-`
-            }
-
-            if (shape.figure === 'LoopEnd') {
-                pascal_code += `end;
-`
-            }
+        while (true) {
+            ShapeToCode(shape)
 
             if (shape.figure === 'End') {
-                pascal_code += `end.`
+                ace.edit('editorCode').setValue(code)
                 break
             }
 
             if (shape.figure === 'Ref') {
-                shape = shapes.find(item => item.text === shape.text && item.key !== shape.key)
-                if (!shape) break
+                let second_shape = shapes.filter(item => item.text === shape.text && item.key !== shape.key)
+                if (second_shape.length === 0) {
+                    alert(`Не найдена вторая ссылка со значением "${shape.text}"`)
+                    break
+                }
+                if (second_shape.length > 1) {
+                    alert(`Ссылок со значением "${shape.text}" больше двух`)
+                    break
+                }
+                shape = second_shape[0]
             }
 
-            const link = links.find(item => item.from === shape.key)
-            if (!link) break
+            let link = links.filter(item => item.from === shape.key)
+            if (link.length === 0) {
+                alert('Не обнаружен блок "Конец"')
+                break
+            }
+            if (link.length > 1) {
+                alert('Обнаружено несколько выходов из блока, не позволяющего ветвление')
+                break
+            }
+            link = link[0]
 
             shape = shapes.find(item => item.key === link.to)
-        } while (shape)
-
-        ace.edit('editorCode').setValue(pascal_code)
+            if (!shape) {
+                alert('Не обнаружен блок "Конец"')
+                break
+            }
+        }
     })
 })
