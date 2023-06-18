2dd059f6e48628fb47c6aaec7463206f00b031c6
anarh <anarhist290@mail.ru>
2023-06-18 18:49:39 +0300

    Доделывает запуск кода

--- execute/execute.js
+++ execute/execute.js
@@ -6,8 +6,12 @@ $(() => {
                 code: ace.edit('editorCode').getValue(),
                 input: '',
             },
+            dataType: 'JSON',
             success: response => {
-                $('.editor__console').html(response)
+                $('.editor__console').html(`
+                    <span class="compilation_output">${response['compilation_output']}</span>
+                    <span class="execution_output">${response['execution_output']}</span>
+                `)
             },
         })
     })
--- execute/execute.php
+++ execute/execute.php
@@ -9,6 +9,18 @@ if (!is_dir('temp')) {
 file_put_contents('temp\program.pas', $code);
 file_put_contents('temp\input.txt', $input);
 
-shell_exec('chcp 65001');
-$output = shell_exec('fpc .\temp\program.pas > .\temp\program.log && type .\temp\input.txt | .\temp\program');
-echo nl2br($output);
+if (stripos(php_uname(), 'win') !== false) {
+    shell_exec('chcp 65001');
+    $compilation_output = shell_exec('fpc .\temp\program.pas');
+    $execution_output = shell_exec('type .\temp\input.txt | .\temp\program');
+} else {
+    $compilation_output = shell_exec('fpc .\temp\program.pas');
+    $execution_output = shell_exec('cat .\temp\input.txt | .\temp\program');
+}
+
+$json = [
+    'compilation_output' => nl2br($compilation_output),
+    'execution_output' => nl2br($execution_output),
+];
+
+echo json_encode($json, JSON_UNESCAPED_UNICODE);
